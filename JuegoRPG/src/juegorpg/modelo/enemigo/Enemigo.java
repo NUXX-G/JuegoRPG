980c8d3a74737d57526cbd2715fa238b2cb08016
NUXX-G <nelsonffkarlsson.com>
2026-02-12 11:13:24 +0100

    Enemigo

--- JuegoRPG/src/juegorpg/modelo/enemigo/Enemigo.java
+++ JuegoRPG/src/juegorpg/modelo/enemigo/Enemigo.java
@@ -1,6 +1,30 @@
 package juegorpg.modelo.enemigo;
 
-public class Enemigo 
+import juegorpg.modelo.Entidad;
+
+public class Enemigo extends Entidad
 {
+    private int experienciaAlMorir;
+    private int oroAlMorir;
+    private int dificultad;
+
+    public Enemigo(String nombre, int vidaMaxima, int ataque, int defensa, int nivel, int experienciaAlMorir, int oroAlMorir, int dificultad) 
+    {
+        super(nombre, vidaMaxima, ataque, defensa, nivel);
+        this.experienciaAlMorir = experienciaAlMorir;
+        this.oroAlMorir = oroAlMorir;
+        this.dificultad = dificultad;
+    }
     
+
+
+    @Override
+    public int calcularDanioAtaque() 
+    {
+    }
+
+    @Override
+    public String obtenerDescripcion() 
+    {
+    }
 }
--- JuegoRPG/src/juegorpg/modelo/personaje/Ladron.java
+++ JuegoRPG/src/juegorpg/modelo/personaje/Ladron.java
@@ -25,6 +25,7 @@ public class Ladron extends Personaje
             super.recibirDanio(danio);
         }
     }
+    
     @Override
     protected void alSubirNivel() 
     {
