4ca0e3a0980a60e79ff5b4d0eb58979160a4ba00
NuzzzKa <nelsonffkarlsson@gmail.com>
2026-02-13 17:37:45 +0100

    Fase 3 COMPLETADA

--- JuegoRPG/src/juegorpg/JuegoRPG.java
+++ JuegoRPG/src/juegorpg/JuegoRPG.java
@@ -4,6 +4,11 @@ import juegorpg.modelo.enemigo.BossFinal;
 import juegorpg.modelo.enemigo.Dragon;
 import juegorpg.modelo.enemigo.Goblin;
 import juegorpg.modelo.enemigo.Orco;
+import juegorpg.modelo.item.Arma;
+import juegorpg.modelo.item.Armadura;
+import juegorpg.modelo.item.Consumible;
+import juegorpg.modelo.item.Inventario;
+import juegorpg.modelo.item.Item;
 import juegorpg.modelo.personaje.Arquero;
 import juegorpg.modelo.personaje.Guerrero;
 import juegorpg.modelo.personaje.Ladron;
@@ -126,6 +131,55 @@ public class JuegoRPG
         System.out.println("Danio: " + bossFinal1.calcularDanioAtaque());
         System.out.println(bossFinal1.toString());
         System.out.println("");
+        
+        
+        //Item
+        Inventario inventario1 = new Inventario();
+        Arma arma1 = new Arma("Daga", "Arma corta", 5, 2);
+        Arma arma2 = new Arma("Espada", "Locura es una espada", 15, 10);
+        Arma arma3 = new Arma("Arco", "Arma a distancia", 10, 4);
+        Armadura armadura1 = new Armadura("Botas", "Sirven para proteger los pies", 8, 4);
+        Armadura armadura2 = new Armadura("Peto", "Sirve para proteger el pecho", 15, 10);
+        Consumible consumible1 = new Consumible("Pocion de Mana", "Sirve para recuperar mana", 5, 0, 10);
+        Consumible consumible2 = new Consumible("Pocion de vida", "Sirve para recuperar vida", 5, 10, 0);
+        Consumible consumible3 = new Consumible("Pocion divina", "Sirve para recuperar tanto mana como vida", 15, 10, 10);
+        inventario1.agregarItem(arma1);
+        inventario1.agregarItem(arma2);
+        inventario1.agregarItem(arma3);
+        inventario1.agregarItem(armadura1);
+        inventario1.agregarItem(armadura2);
+        inventario1.agregarItem(consumible1);
+        inventario1.agregarItem(consumible2);
+        inventario1.agregarItem(consumible3);
+        System.out.println("--- MOSTRAR INVENTARIO ---");
+        inventario1.mostrarInventario();
+        System.out.println("");
+        
+        System.out.println("--- MOSTRAR INVENTARIO DESPUES DE ELIMINAR 2 ITEMS ---");
+        inventario1.quitarItem(0);
+        inventario1.quitarItem(4);
+        inventario1.mostrarInventario();
+        System.out.println("");
+        
+        System.out.println("--- INVENTARIO LLENO ---");
+        Arma prueba1 = new Arma("Prueba", "Descripcion Prueba", 0, 0);
+        inventario1.agregarItem(prueba1);
+        inventario1.agregarItem(prueba1);
+        inventario1.agregarItem(prueba1);
+        inventario1.agregarItem(prueba1);
+        inventario1.agregarItem(prueba1);
+        inventario1.agregarItem(prueba1);
+        inventario1.agregarItem(prueba1);
+        inventario1.agregarItem(prueba1);
+        inventario1.agregarItem(prueba1);
+        inventario1.agregarItem(prueba1);
+        inventario1.agregarItem(prueba1);
+        inventario1.agregarItem(prueba1);
+        inventario1.agregarItem(prueba1);
+        inventario1.agregarItem(prueba1);
+        inventario1.agregarItem(prueba1);
+        inventario1.agregarItem(prueba1);
+        inventario1.mostrarInventario();
     }
     
 }
